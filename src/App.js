ded003441ee5e2023cb281233716b4d315e3d036
Edsouzaa <edsonhenriqueabreu@gmail.com>
2025-09-15 21:14:11 -0300

    Finalizado?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Relogio(){
+  // Criar um estaqdo chamado 'hora' que sera inicializado 
+  // com a função Date()
+
+  const [hora, sethora] = useState(new Date());
+
+  // useEffect executado sem dependencias isto é 
+  // o array [] vazio
+
+  useEffect(() =>{
+  // inicia o relogio  setInterval(a função a ser executada, intervalo de tempo)
+  const idIntervalo = setInterval(() =>{
+  console.log('Atualizando o relogio'); 
+  sethora(new Date()); }, 1000);
+  // esse retorno do useEffect() quando o componente for desmontado
+  // evita-se que o setInterval rode desnecessariamente em segundo plano
+  return() =>{
+    clearInterval(idIntervalo);
+    console.log('Relogio parado, intervalo limpo');
+  }
+  }, [])
+
+  return(
+    <div>
+      <h2>Relogio Digital</h2>
+      <p>A hora atual é: {hora.toLocaleTimeString()}</p>
     </div>
-  );
-}
+  )
 
-export default App;
+}
+export default Relogio;
